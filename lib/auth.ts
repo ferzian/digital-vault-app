e3e68c66f8db8b3a66c6eda858600ea7b256a0b1
ferzian <m.ferzian09@gmail.com>
2026-08-31 11:37:44 +0700

    feat: implement authentication system with Better Auth, email/password, Google provider, and admin role support

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/prisma";
+import { admin } from "better-auth/plugins";
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
@@ -17,4 +18,10 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [
+    admin({
+      defaultRole: "user",
+      adminRole: "admin",
+    }),
+  ],
 });
