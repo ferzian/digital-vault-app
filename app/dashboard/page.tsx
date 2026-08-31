9b547a7478ef2cfaad2fba32acbf5caa16d5e6f8
ferzian <m.ferzian09@gmail.com>
2026-08-31 13:06:21 +0700

    feat: implement authentication infrastructure with Better Auth and create dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -24,7 +24,7 @@ const DashboardPage = () => {
   return (
     <main className="max-w-md h-screen flex items-center justify-center flex-col mx-auto p-6 space-y-4 text-white">
       <h1 className="text-2xl font-bold">Dashboard</h1>
-      <p>Welcome, {user.name || "User"}!</p>
+      <p>Welcome, you're {user.role}, {user.name || "User"}!</p>
       <p>Email: {user.email}</p>
       <button
         onClick={() => signOut()}
--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,8 @@ import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL,
-    plugins: [adminClient()],
-})
-export const {signIn, signUp, signOut, useSession} = createAuthClient();
+  baseURL: process.env.BETTER_AUTH_URL,
+  plugins: [adminClient()],
+});
+
+export const { signIn, signUp, signOut, useSession } = authClient;
