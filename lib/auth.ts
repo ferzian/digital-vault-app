3ad303b7257f6d6e6386ce05be164c938a1d64a1
ferzian <m.ferzian09@gmail.com>
2026-08-31 10:55:45 +0700

    feat: implement Better Auth with Google OAuth and email/password providers

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,20 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/prisma";
+import { GoogleOptions } from "better-auth";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   emailAndPassword: {
-    enabled: true, 
-  }, 
+    enabled: true,
+  },
 });
